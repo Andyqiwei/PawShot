235aa62a948426cf85022cc680db09d50effce36
Andy Liu <andyqiwei11@gmail.com>
2026-02-07 21:40:39 -0500

    Initial Commit

---
+++ PawShot/PawShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawShotApp.swift
+//  PawShot
+//
+//  Created by Andy Liu on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct PawShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
